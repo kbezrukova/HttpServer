097c5775b2bde261f25f555527f37e89a648c692
kbezrukova <95983771+kbezrukova@users.noreply.github.com>
2022-03-13 21:50:24 +0300

    HttpServer
    HttpServer

---
+++ src/letscode/Main.java
@@ -0,0 +1,10 @@
+package letscode;
+
+public class Main {
+
+    public static void main(String[] args) {
+        new Server((req, resp)->{
+            return "<html><body><h1>Hello, naive</h1></body></html>";
+        }).bootstrap();
+    }
+}
